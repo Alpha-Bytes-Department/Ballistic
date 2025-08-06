112562e433db6a04abb72d2e0d1d2789a636260e
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-08-07 02:50:13 +0600

    bug fixed

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,10 +1,12 @@
 import BallisticCalculator from '@/components/Calculator/BallisticCalculator/BallisticCalculator';
 import Features from '@/components/Features/Features';
+import Banner from '@/components/LandingPage/Banner';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
+            <Banner/>
             <BallisticCalculator design="my-10"/>
             <Features/>
         </div>

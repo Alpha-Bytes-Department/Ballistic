336f284e4547160a85decbc99ae67ac0c1e06f0f
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-08-16 23:40:33 +0600

    change bg image

--- components/LandingPage/BannerUpdate.jsx
+++ components/LandingPage/BannerUpdate.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const BannerUpdate = () => {
     return (
-        <div className="bg-[url('/images/banner.jpg')] h-[600px] w-full bg-cover">
-            <h1 className='text-3xl text-center text-white '>DISCOVER THE KLEAR DIFFERENCE</h1>
+        <div className="bg-[url('/images/banner.jpg')] h-[600px] w-full bg-cover flex justify-center items-center my-5">
+            <h1 className='text-3xl md:text-5xl lg:text-7xl text-center text-white font-bold'>DISCOVER THE KLEAR <br/> DIFFERENCE</h1>
         </div>
     );
 };
